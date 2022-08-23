19cb4c8d09eb1791e2f1c10c76ba43b41653733a
Melih Can Udul <45386070+MelihCanUdul@users.noreply.github.com>
2022-08-23 09:32:25 +0300

    Departments classes added

---
+++ DapperProject/Departments.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DapperProject
+{
+    public class Departments
+    {
+        public int DepartmentsId { get; set; }
+        public string Department { get; set; }
+        public string Description { get; set; }
+    }
+}
